227d3764c3b93bc0d58631f0148e2f01d3cfd7c3
msastoques <msastoque@cips.edu.co>
2023-11-02 23:53:03 -0500

    listo creacion de documentos sin reemplazo de texto

--- Codigo.js
+++ Codigo.js
@@ -16,7 +16,63 @@ function mostrarBarraLateral() {
 }
 
 function generarDocumentos() {
-    let hojaActual = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    //obteber planeador-plantilla
+    const plantilla = DriveApp.getFileById('1xhr4df2XdAXws9ghTIxqdHzJ0A1oI-WatCyzGRDqcRw');
+
+    let fila = 2;
+    
+    let hojaActual = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+    let rango = hojaActual.getRange('A' + fila + ':Z' + fila);
+
+    
+    let celda = rango.getCell(1, 1); // getCell(1,1) retorna A2
+    
+
+    //lista de variables para combinar correspondencia
+    let estado ;
+    let grado  ;
+    let periodo;
+    let semanas;
+
+
+
+    while(!celda.isBlank()){
+        //obtener todos los datos de la fila
+        estado  = rango.getCell(1, 1).getValue();
+        grado   = rango.getCell(1, 2).getValue();
+        periodo = rango.getCell(1, 3).getValue();
+        semanas = rango.getCell(1, 4).getValue();
+
+        if(estado == 'Impreso')continue;
+
+        rango.getCell(1, 1).setValue('Impreso');
+
+        //Creamos documento copia de la plantilla
+        const docNuevo = plantilla.makeCopy(grado + '-planeador-tecnologia-e-informatica-periodo-' + periodo + '-semanas-' + reemplazarComasYEspaciosPorGuiones(semanas));
+        const documento = DocumentApp.openById(docNuevo.getId());
+        const body = documento.getBody();
+
+        //obtener y reemplazar datos
+
+        //siguiente fila
+        fila++;
+        rango = hojaActual.getRange('A' + fila + ':Z' + fila);        
+        celda = rango.getCell(1, 1);
+    }
+
+}
+
+//funciones de prueba
+
+//traer el número de columnas de un rango
+function getNumCol() {
+
+    DriveApp.getFileById('1xhr4df2XdAXws9ghTIxqdHzJ0A1oI-WatCyzGRDqcRw');
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheets()[0];
+    
+    var range = sheet.getRange("B2:D5");
+    Logger.log(range.getNumColumns());
 }
 
 
--- herramientas.js
+++ herramientas.js
@@ -14,3 +14,29 @@ function literalNumero(numero) {
       return "Número fuera de rango";
     }
   }
+
+
+
+  function reemplazarComasYEspaciosPorGuiones(texto) {
+    // Utiliza la función replace con una expresión regular para reemplazar comas y espacios por guiones.
+    const textoModificado = texto.replace(/[, ]/g, '-');
+    return textoModificado;
+  }
+
+/**
+ * Reemplaza comas y espacios por guiones en un texto.
+ *
+ * @param {string} texto - El texto original en el que se realizarán los reemplazos.
+ * @returns {string} El texto modificado con comas y espacios reemplazados por guiones.
+ */
+function reemplazarComasYEspaciosPorGuiones(texto) {
+  // Utiliza la función replace con una expresión regular para reemplazar comas y espacios por guiones.
+  const textoModificado = texto.replace(/ y /g, '-').replace(/[, ]/g, '-').replace(/--/g, '-');
+  return textoModificado;
+}
+//probar
+// const textoOriginal = "1,2, 3 y 4";
+// const textoModificado = reemplazarComasYEspaciosPorGuiones(textoOriginal);
+// console.log(textoOriginal)
+// console.log(textoModificado)
+
